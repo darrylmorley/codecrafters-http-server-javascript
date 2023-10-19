2f35c68c853187cb1309dec5c83bef6d13075b26
Darryl Morley <darrylmorley.uk@gmail.com>
2023-10-19 06:59:43 +0100

    3rd pass

--- app/main.js
+++ app/main.js
@@ -2,8 +2,14 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log(data.toString());
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    let path = data.toString().split(" ");
+
+    if (path[1] === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
     socket.end();
   });
 
