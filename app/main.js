a175eef63713d1e905d3256c532b8de157149700
Darryl Morley <darrylmorley.uk@gmail.com>
2023-10-19 07:04:59 +0100

    4th pass

--- app/main.js
+++ app/main.js
@@ -6,6 +6,12 @@ const server = net.createServer((socket) => {
 
     if (path[1] === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path[1].startsWith("/echo/")) {
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${
+          path[1].slice(6).length
+        }\r\n\r\n${path[1].slice(6)}`
+      );
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
