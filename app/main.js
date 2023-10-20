25a073c0d96197882aa7ccf3a6a213734ac73f5f
Darryl Morley <darrylmorley.uk@gmail.com>
2023-10-20 20:27:52 +0100

    pass 7

--- app/main.js
+++ app/main.js
@@ -1,11 +1,13 @@
 const net = require("net");
+const fs = require("fs");
+
+const dirFlag = process.argv.indexOf("--directory");
+const dir = dirFlag > 1 && process.argv[process.argv.indexOf("--directory") + 1];
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     let path = data.toString().split(" ");
     let agent = data.toString().split("\r\n")[2];
-    console.log(agent);
-    console.log(agent.slice(12));
 
     if (path[1] === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
@@ -21,6 +23,19 @@ const server = net.createServer((socket) => {
           agent.slice(12).length
         }\r\n\r\n${agent.slice(12)}`
       );
+    } else if (path[1].startsWith("/files/")) {
+      let fileName = path[1].slice(7);
+      let filePath = `${dir}${fileName}`;
+
+      if (fs.existsSync(filePath)) {
+        let data = fs.readFileSync(filePath, "utf-8");
+
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`
+        );
+      } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
